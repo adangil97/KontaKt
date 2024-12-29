c55bf7c62a1d1ef3d4da0e6619cf700dbd2edc81
Adán Castillo <adan.gil97@gmail.com>
2024-12-29 10:14:38 -0600

    feat(Slide Transition): Integrate Slide Transition between screens

--- composeApp/src/commonMain/kotlin/com/example/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/App.kt
@@ -2,20 +2,28 @@ package com.example
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.annotation.ExperimentalVoyagerApi
 import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.NavigatorDisposeBehavior
+import cafe.adriel.voyager.transitions.SlideTransition
 import com.example.contacts.presentation.list.ContactListScreen
 import kontakt.composeapp.generated.resources.Res
 import kontakt.composeapp.generated.resources.global_navigator_key
 import org.jetbrains.compose.resources.stringResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
-@Preview
+@OptIn(ExperimentalVoyagerApi::class)
 fun App() {
     MaterialTheme {
         Navigator(
             ContactListScreen(),
-            key = stringResource(Res.string.global_navigator_key)
-        )
+            key = stringResource(Res.string.global_navigator_key),
+            disposeBehavior = NavigatorDisposeBehavior(disposeSteps = false)
+        ) { navigator ->
+            SlideTransition(
+                navigator = navigator,
+                disposeScreenAfterTransitionEnd = true
+            )
+        }
     }
 }
--- composeApp/src/commonMain/kotlin/com/example/contacts/presentation/detail/ContactDetailNavigationScreen.kt
+++ composeApp/src/commonMain/kotlin/com/example/contacts/presentation/detail/ContactDetailNavigationScreen.kt
@@ -1,13 +1,25 @@
 package com.example.contacts.presentation.detail
 
 import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.annotation.ExperimentalVoyagerApi
 import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.NavigatorDisposeBehavior
+import cafe.adriel.voyager.transitions.SlideTransition
 import com.example.core.ContactUniqueScreen
 
+@OptIn(ExperimentalVoyagerApi::class)
 class ContactDetailNavigationScreen(private val contactId: Long? = null) : ContactUniqueScreen() {
 
     @Composable
     override fun Content() {
-        Navigator(ContactDetailScreen(contactId))
+        Navigator(
+            ContactDetailScreen(contactId),
+            disposeBehavior = NavigatorDisposeBehavior(disposeSteps = false)
+        ) { navigator ->
+            SlideTransition(
+                navigator = navigator,
+                disposeScreenAfterTransitionEnd = true
+            )
+        }
     }
 }
