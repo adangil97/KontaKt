40c35475d88253ae30afbdea7da68fb08acde566
Adán Castillo <adan.gil97@gmail.com>
2024-12-29 14:16:28 -0600

    refactor(Remove Static imports): Remove static imports

--- composeApp/src/commonMain/kotlin/com/example/contacts/framework/ContactModule.kt
+++ composeApp/src/commonMain/kotlin/com/example/contacts/framework/ContactModule.kt
@@ -2,8 +2,10 @@ package com.example.contacts.framework
 
 import com.example.contacts.data.ContactRepository
 import com.example.contacts.presentation.detail.ContactDetailViewModel
+import com.example.contacts.presentation.list.ContactListViewModel
 import com.example.contacts.usecases.DeleteContactById
 import com.example.contacts.usecases.GetContactById
+import com.example.contacts.usecases.GetContacts
 import com.example.contacts.usecases.SaveContact
 import com.example.contacts.usecases.SearchContacts
 import com.example.contacts.usecases.UpdateContact
@@ -13,10 +15,10 @@ val contactModule = module {
     single<ContactRepository> { DBContactRepository(get()) }
     single { DeleteContactById(get()) }
     single { GetContactById(get(), get()) }
-    single { com.example.contacts.usecases.GetContacts(get()) }
+    single { GetContacts(get()) }
     single { SaveContact(get(), get(), get()) }
     single { SearchContacts(get()) }
     single { UpdateContact(get()) }
     factory { ContactDetailViewModel(get(), get(), get(), get(), get(), get()) }
-    factory { com.example.contacts.presentation.list.ContactListViewModel(get(), get()) }
+    factory { ContactListViewModel(get(), get()) }
 }
